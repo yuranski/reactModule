b4c10e1a55644ecf71ad407ab0d1dc52c2022551
yuranski <yuriibidniak@gmail.com>
2021-08-14 12:28:38 +0300

    Lesson3 Full

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,29 +2,26 @@ import {useEffect, useState} from "react";
 import {getUserPosts, getUsers} from "../../services/user.services";
 import {User} from "../user/User";
 
-export function Users(){
+export function Users() {
 
     let [users, setUsers] = useState([])
-    let [userPost, setUserPost] = useState([])
+    let [userPost, setUserPost] = useState(null)
 
     useEffect(() => {
         getUsers().then(value => setUsers([...value]))
-    },[])
+    }, [])
 
-    function posts(id){
-        getUserPosts(id).then(value =>
-        {
+    function posts(id) {
+        getUserPosts(id).then(value => {
             setUserPost([...value])
         })
     }
 
-    console.log(users)
-
-    return(
+    return (
         <div>
             {users.map(value => <User key={value.id} item={value} posts={posts}/>)}
             //TODO Key не знаю как вписати Help!
-            {userPost && userPost.map((value,index) => <p>{value.body}</p>)}
+            {userPost && userPost.map((value, index) => <p>{value.body}</p>)}
         </div>
     )
 }
