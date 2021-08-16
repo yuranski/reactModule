badc9858e2ba03f37924b23e47c16be0efafafd7
yuranski <yuriibidniak@gmail.com>
2021-08-16 20:59:05 +0300

    Lesson4 full

--- src/components/cars/CarsForm.js
+++ src/components/cars/CarsForm.js
@@ -1,68 +1,86 @@
 import {useEffect, useState} from "react";
 import {Car} from "../car/Car";
 import {getCars, saveValueCars} from "../../services/car.services";
+import './carForm.css';
 
-export function CarsForm(){
+export function CarsForm() {
 
-    let [cars,setCars] = useState([])
+    let [cars, setCars] = useState([])
 
-    let [model,setModels] = useState([])
-    let [price,setPrice] = useState([])
-    let [year,setYear] = useState([])
+    let [model, setModels] = useState([])
+    let [price, setPrice] = useState([])
+    let [year, setYear] = useState([])
 
-    let [value,setValue] = useState({model:'',price:'',year:''})
+    let [value, setValue] = useState({model: '', price: '', year: ''})
 
-    let [lastIndexArr,setLastIndexArr] = useState([])
+    let [lastIndexArr, setLastIndexArr] = useState([])
 
     useEffect(() => {
         getCars().then(value => {
             setCars([...value])
             setLastIndexArr([...value])
         })
-    },[])
+    }, [])
 
+    const [disableModel, setDisableModel] = useState(true);
+    const [disablePrice, setDisablePrice] = useState(true);
+    const [disableYear, setDisableYear] = useState(true);
 
     const onSubForm = (e) => {
-        e.preventDefault()
-        let tempValue = {model,price,year}
+        // e.preventDefault()
+        let tempValue = {model, price, year}
         setValue({...tempValue})
         saveValueCars(tempValue).then()
-
-        console.log(model.length)
     }
 
-
+    console.log(disableModel, disablePrice, disableYear)
 
     const onInputChangeModel = (e) => {
-        setModels(e.target.value)
-
-        if(model.length <= 20){
-            console.log('asd')
+        if (e.target.value.length <= 20 && e.target.value.length >= 1) {
+            setDisableModel(false)
+            setModels(e.target.value)
+            e.target.className = "errorValidGreen"
         } else {
-            console.log('333')
+            setDisableModel(true)
+            e.target.className = "errorValidRed"
         }
     }
     const onInputChangePrice = (e) => {
-        setPrice(e.target.value)
-    }
-    const onInputChangeYear = (e) => {
-        setYear(e.target.value)
+        if (e.target.value > 0) {
+            setDisablePrice(false)
+            setPrice(e.target.value)
+            e.target.className = "errorValidGreen"
+        } else {
+            setDisablePrice(true)
+            e.target.className = "errorValidRed"
+        }
     }
 
-    return(
+    const onInputChangeYear = (e) => {
+        if (e.target.value >= 1990 && e.target.value <= 2021) {
+            setDisableYear(false)
+            setYear(e.target.value)
+            e.target.className = "errorValidGreen"
+        } else {
+            setDisableYear(true)
+            e.target.className = "errorValidRed"
+        }
+    }
+
+    return (
         <div>
             <form onSubmit={onSubForm}>
-                <input type="text" name={model}  onInput={onInputChangeModel} placeholder="Модель машини"/>
+                <input type="text" name={model} onInput={onInputChangeModel} placeholder="Модель машини"/>
                 <input type="text" name={price} onInput={onInputChangePrice} placeholder="Ціна машини"/>
                 <input type="text" name={year} onInput={onInputChangeYear} placeholder="Рік машини"/>
-                <button>Submit</button>
+                <button disabled={disableModel || disablePrice || disableYear} id='btn'>Submit</button>
             </form>
 
-            {cars.filter(value => value.id > lastIndexArr.length + 75).map(value =>  <Car key={value.id}
-                                                                      id={value.id}
-                                                                      model={value.model}
-                                                                      price={value.price}
-                                                                      year={value.year}/>)}
+            {cars.filter(value => value.id > lastIndexArr.length + 75).map(value => <Car key={value.id}
+                                                                                         id={value.id}
+                                                                                         model={value.model}
+                                                                                         price={value.price}
+                                                                                         year={value.year}/>)}
         </div>
     )
 }
