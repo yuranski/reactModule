a6a34214989674d4740435e6609d6addcf2ab463
yuranski <yuriibidniak@gmail.com>
2021-08-29 12:41:38 +0300

    Lesson8 REDUX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    let inc = useSelector(({counter}) => counter)
+    let users = useSelector(({users}) => users)
+    let dispatch = useDispatch()
+
+
+    function increment() {
+        dispatch({type: "INC"})
+    }
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => dispatch({type: 'USERS', payload: value}))
+    },[])
+
+    return (
+        <div>
+
+            <button onClick={increment}>+</button>
+            <h1>{inc}</h1>
+            <div>{users.map(value => <div key={value.id}>{value.name} </div>)}</div>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+import {Provider} from "react-redux";
+
+
+const initialState = {
+    counter: 0,
+    users: []
+}
+
+const rootReducer = (state = initialState, actions) => {
+    switch (actions.type) {
+        case "INC": {
+            return {...state, counter: state.counter + 1}
+        }
+        case "USERS": {
+            return {...state, users: actions.payload}
+        }
+        default:
+            return state
+    }
+}
+
+
+let store = createStore(rootReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
